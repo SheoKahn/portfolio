935cf7382588099461384c9144fba5b029cfa515
José L Silva <joseluis@sheokahn.com>
2024-07-26 11:55:32 -0500

    feat(dependencies): remove tailwindcss

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
 	output: 'static',
 	site: 'https://sheokahn.com/',
 	devToolbar: { enabled: false },
-	integrations: [tailwind()],
 })
